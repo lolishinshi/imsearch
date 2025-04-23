ee1aa53daeb4ca8b3dd3b117db42f1db266f8be2
Aloxaf Yin <Aloxafx@gmail.com>
2025-04-23 15:58:30 +0800

    misc: 清理无用代码

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -1,9 +1,10 @@
 use std::convert::TryInto;
-use std::mem;
 
 use crate::db::database::ImageColumnFamily;
 use crate::db::database::MetaData;
-use rocksdb::{BlockBasedOptions, DBCompressionType, Error, Options, DB, BlockBasedIndexType, Cache};
+use rocksdb::{
+    BlockBasedIndexType, BlockBasedOptions, Cache, DBCompressionType, Error, Options, DB,
+};
 
 pub fn default_options() -> Options {
     let mut block_opts = BlockBasedOptions::default();
@@ -15,7 +16,6 @@ pub fn default_options() -> Options {
     block_opts.set_cache_index_and_filter_blocks(true);
     block_opts.set_pin_top_level_index_and_filter(true);
     block_opts.set_pin_l0_filter_and_index_blocks_in_cache(true);
-
 
     let mut options = Options::default();
     options.set_block_based_table_factory(&block_opts);
@@ -47,36 +47,6 @@ pub fn init_column_family(db: &DB) -> Result<(), Error> {
     Ok(())
 }
 
-pub trait CompactBytes {
-    fn to_cpt_bytes(&self) -> &[u8];
-    fn from_cpt_bytes(b: &[u8]) -> Self;
-}
-
-macro_rules! impl_compact_bytes {
-    ($($t:ty),*) => {
-        $(impl CompactBytes for $t {
-            #[inline]
-            fn to_cpt_bytes(&self) -> &[u8] {
-                unsafe {
-                    let b = mem::transmute::<_, &[u8; (Self::BITS / 8) as usize]>(self);
-                    // SAFETY: `index` is between 0..=8
-                    let index = Self::BITS / 8 - self.leading_zeros() / 8;
-                    b.get_unchecked(..index as usize)
-                }
-            }
-
-            #[inline]
-            fn from_cpt_bytes(bytes: &[u8]) -> Self {
-                let mut buf = [0u8; (Self::BITS / 8) as usize];
-                buf[..bytes.len()].copy_from_slice(bytes);
-                Self::from_le_bytes(buf)
-            }
-        })*
-    }
-}
-
-impl_compact_bytes!(i32, u64);
-
 pub fn bytes_to_u64<T: AsRef<[u8]>>(bytes: T) -> u64 {
     u64::from_le_bytes(
         bytes
@@ -93,32 +63,4 @@ pub fn bytes_to_i32<T: AsRef<[u8]>>(bytes: T) -> i32 {
             .try_into()
             .expect("byte cannot convert to i32"),
     )
-}
-
-#[cfg(test)]
-mod tests {
-    use super::CompactBytes;
-
-    #[test]
-    fn to_bytes() {
-        assert_eq!(1i32.to_cpt_bytes(), &[1]);
-        assert_eq!(255i32.to_cpt_bytes(), &[255]);
-        assert_eq!(256u64.to_cpt_bytes(), &[0, 1]);
-        assert_eq!(65535u64.to_cpt_bytes(), &[255, 255]);
-        assert_eq!(2147483647i32.to_cpt_bytes(), &[255, 255, 255, 127]);
-    }
-
-    #[test]
-    fn from_bytes() {
-        assert_eq!(u64::from_cpt_bytes(&[1]), 1);
-        assert_eq!(i32::from_cpt_bytes(&[255, 255, 255, 127]), 2147483647);
-    }
-
-    #[test]
-    fn convert_between_u64_bytes() {
-        for i in 0..(1 << 16) {
-            let b = i.to_cpt_bytes();
-            assert_eq!(u64::from_cpt_bytes(b), i);
-        }
-    }
 }
