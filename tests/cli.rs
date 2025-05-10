f7526e5aae268c7c6db4d88db9f3bcad55ed869c
Aloxaf Yin <Aloxafx@gmail.com>
2025-05-10 22:18:10 +0800

    tests: 优化测试

--- tests/cli.rs
+++ tests/cli.rs
@@ -27,33 +27,47 @@ macro_rules! cmd {
     }};
 }
 
-fn file_from_dir(dir: &str) -> DirEntry {
-    WalkDir::new(dir)
+fn file_from_dir(dir: &str) -> Result<DirEntry> {
+    Ok(WalkDir::new(dir)
         .into_iter()
         .filter(|x| x.as_ref().unwrap().file_type().is_file())
         .next()
-        .unwrap()
-        .unwrap()
+        .unwrap()?)
 }
 
-#[rstest]
-#[case("-b=500")]
-#[case("--on-disk")]
-fn build_and_search(#[case] arg: &str) -> Result<()> {
+#[test]
+fn add_tar() -> Result<()> {
+    let conf_dir = assert_fs::TempDir::new()?;
+    let tar_path = conf_dir.path().join("part1.tar");
+
+    fs::copy("tests/index.template", conf_dir.path().join("index.template"))?;
+
+    cmd!("tar", "cf", &tar_path, "tests/dataset/part1").success();
+
+    cargo_run!("imsearch", "-c", conf_dir.path(), "add", tar_path).success();
+    cargo_run!("imsearch", "-c", conf_dir.path(), "build").success();
+
+    let part1 = file_from_dir("tests/dataset/part1")?;
+
+    cargo_run!("imsearch", "-c", conf_dir.path(), "search", part1.path())
+        .stdout(predicate::str::contains(part1.path().to_str().unwrap()));
+
+    Ok(())
+}
+
+#[test]
+fn search_no_merge() -> Result<()> {
     let conf_dir = assert_fs::TempDir::new()?;
 
     fs::copy("tests/index.template", conf_dir.path().join("index.template"))?;
 
-    cmd!("tar", "cf", conf_dir.path().join("part2.tar"), "tests/dataset/part2").success();
+    let part1 = file_from_dir("tests/dataset/part1")?;
+    let part2 = file_from_dir("tests/dataset/part2")?;
 
     cargo_run!("imsearch", "-c", conf_dir.path(), "add", "tests/dataset/part1").success();
-    cargo_run!("imsearch", "-c", conf_dir.path(), "add", conf_dir.path().join("part2.tar"))
-        .success();
-
-    cargo_run!("imsearch", "-c", conf_dir.path(), "build", arg).success();
-
-    let part1 = file_from_dir("tests/dataset/part1");
-    let part2 = file_from_dir("tests/dataset/part2");
+    cargo_run!("imsearch", "-c", conf_dir.path(), "build").success();
+    cargo_run!("imsearch", "-c", conf_dir.path(), "add", "tests/dataset/part2").success();
+    cargo_run!("imsearch", "-c", conf_dir.path(), "build", "--no-merge").success();
 
     cargo_run!("imsearch", "-c", conf_dir.path(), "search", part1.path())
         .stdout(predicate::str::contains(part1.path().to_str().unwrap()));
@@ -64,11 +78,30 @@ fn build_and_search(#[case] arg: &str) -> Result<()> {
 }
 
 #[rstest]
-#[case("-b=500", "-b=500")]
-#[case("--on-disk", "-b=500")]
-#[case("-b=500", "--on-disk")]
-#[case("--on-disk", "--on-disk")]
-fn build_multi_times(#[case] arg1: &str, #[case] arg2: &str) -> Result<()> {
+#[case::onmemory("-b=500")]
+#[case::ondisk("--on-disk")]
+fn build_1_round(#[case] arg: &str) -> Result<()> {
+    let conf_dir = assert_fs::TempDir::new()?;
+
+    fs::copy("tests/index.template", conf_dir.path().join("index.template"))?;
+
+    cargo_run!("imsearch", "-c", conf_dir.path(), "add", "tests/dataset/part1").success();
+    cargo_run!("imsearch", "-c", conf_dir.path(), "build", arg).success();
+
+    let part1 = file_from_dir("tests/dataset/part1")?;
+
+    cargo_run!("imsearch", "-c", conf_dir.path(), "search", part1.path())
+        .stdout(predicate::str::contains(part1.path().to_str().unwrap()));
+
+    Ok(())
+}
+
+#[rstest]
+#[case::onmemory_then_onmemory("-b=500", "-b=500")]
+#[case::ondisk_then_onmemory("--on-disk", "-b=500")]
+#[case::onmemory_then_ondisk("-b=500", "--on-disk")]
+#[case::ondisk_then_ondisk("--on-disk", "--on-disk")]
+fn build_2_round(#[case] arg1: &str, #[case] arg2: &str) -> Result<()> {
     let conf_dir = assert_fs::TempDir::new()?;
 
     fs::copy("tests/index.template", conf_dir.path().join("index.template"))?;
@@ -78,8 +111,8 @@ fn build_multi_times(#[case] arg1: &str, #[case] arg2: &str) -> Result<()> {
     cargo_run!("imsearch", "-c", conf_dir.path(), "add", "tests/dataset/part2").success();
     cargo_run!("imsearch", "-c", conf_dir.path(), "build", arg2).success();
 
-    let part1 = file_from_dir("tests/dataset/part1");
-    let part2 = file_from_dir("tests/dataset/part2");
+    let part1 = file_from_dir("tests/dataset/part1")?;
+    let part2 = file_from_dir("tests/dataset/part2")?;
 
     cargo_run!("imsearch", "-c", conf_dir.path(), "search", part1.path())
         .stdout(predicate::str::contains(part1.path().to_str().unwrap()));
